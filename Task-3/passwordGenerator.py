f9771683c122ba59bd0e71f84a50caaeda0d0948
RadhikaP <153422450+RadsZX@users.noreply.github.com>
2024-04-09 21:57:07 +0530

    Create passwordGenerator.py

---
+++ Task-3/passwordGenerator.py
@@ -0,0 +1,21 @@
+import string
+import random
+
+if __name__ == "__main__":
+
+    set1=string.ascii_lowercase
+    set2=string.ascii_uppercase
+    set3=string.digits
+    set4=string.punctuation
+    
+    setAll=set1+set2+set3+set4
+
+    passlen=int(input("Enter the length of password you want:"))
+    
+    print("Generated Password:")
+
+    print("".join(random.sample(setAll,passlen)))
+    
+    
+
+
